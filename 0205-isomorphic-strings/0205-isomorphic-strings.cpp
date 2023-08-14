6858cdcb9012a4971e13921c453e92daf5e6714f
Adrish Bhattacharya <88700100+iadrish@users.noreply.github.com>
2023-08-14 21:44:02 +0530

    Time: 9 ms (42.60%), Space: 7.2 MB (8.17%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,int>mp1, mp2;
-        int n = s.length();
-        int m = t.length();
+        unordered_map<char,int>mp1,mp2;
+        int n=s.length();
+        int m=t.length();
         
-        if(n != m){  //if there size is not the same no need to go furthur simply return false
-           return false; 
-        }
         
-        for(int i=0; i<n; i++){
-            
-            if(mp1[s[i]] != mp2[t[i]])  //we map the char with the index+1
+        if(n!=m)
+            return false;
+        for(int i=0;i<n;i++)
+        {
+            if(mp1[s[i]]!=mp2[t[i]])
                 return false;
             
-            mp1[s[i]] = i+1;  //now why do we need to do index+1?
-			mp2[t[i]] = i+1;  //try s="ppaer"  and t="title"  Got the ans?
-            
+            mp1[s[i]]=i+1;
+            mp2[t[i]]=i+1;
         }
-       return true; 
+        return true;
     }
 };
