e813e5cb84abd2891745780b93f1fe8337db8f87
Adrish Bhattacharya <88700100+iadrish@users.noreply.github.com>
2023-10-13 00:48:05 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (27.99%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array.cpp
@@ -0,0 +1,68 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int peak(MountainArray & arr, int n){
+    int start=0,end=n-1;
+    int res=-1;
+
+    while(start<end){
+        int mid=start + (end-start)/2;
+        if(arr.get(mid+1)>arr.get(mid)) 
+            start=mid+1,res=mid+1;
+        else
+         end=mid;
+    }
+    return res;
+}
+
+int bs(int s,int e, MountainArray & arr, int target, bool flag){
+    int res=-1;
+
+    while(s<=e){
+
+        int mid = s + (e-s)/2;
+        if(target== arr.get(mid)){
+            res=mid;
+            if(flag) 
+                s=mid+1;
+            else e=mid-1;
+        }
+        else if(target>arr.get(mid)){
+            if(flag) 
+                e=mid-1;
+            else 
+                s=mid+1;
+        }
+        else{
+            if(flag) s=mid+1;
+            else e=mid-1;
+        }
+    }
+    return res;
+}
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+          int n=mountainArr.length();
+          if(n<3) 
+        return -1;
+
+       int part = peak(mountainArr,n); 
+
+       int l = bs(0,part,mountainArr,target,0);
+
+       if(l!=-1) 
+        return l;
+        
+       int r = bs(part,n-1,mountainArr,target,1);
+       return r;
+        
+    }
+};
