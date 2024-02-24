a415b83c3425ffbab873b40464ff5520df68c973
Adrish Bhattacharya <88700100+iadrish@users.noreply.github.com>
2024-02-24 20:46:50 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (25.18%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -11,27 +11,30 @@ public:
     
     int pop() {
         if(q1.empty())return -1;
-        int el=q1.back();
+        
+        int e1=q1.back();
         int i=0;
-        while(i<q1.size()-1){
-                q2.push(q1.front());
+        while(i<q1.size()-1)
+        {
+            q2.push(q1.front());
             
             q1.pop();
         }
         q1.pop();
-       while(!q2.empty()){
+        
+         while(!q2.empty()){
            q1.push(q2.front());
            q2.pop();
        }
-        return el;
+        return e1;
     }
     
     int top() {
-        return q1.back();
+      return q1.back();  
     }
     
     bool empty() {
-        if(q1.empty()) return true;
+        if(q1.empty())return true;
         
         return false;
     }
