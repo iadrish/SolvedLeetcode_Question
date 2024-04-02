ab70c5e38616517b5f25a89d0e9243c9d7c441aa
Adrish Bhattacharya <88700100+iadrish@users.noreply.github.com>
2024-04-02 10:12:02 +0530

    Time: 10 ms (44.89%), Space: 8.5 MB (82.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,23 +3,10 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        bool x=true;
-        if(s.length()!=t.length())
-        {  
-            x=false;
-        }
-        else
-        {    
-        for(int i=0;i<s.length();i++)
-        {
-            if(s[i]!=t[i])
-            {
-                x=false;
-                break;
-            }
-        }
-        }
         
-        return x;
+        if(s==t) return true;
+        else
+            return false;
+            
     }
 };
