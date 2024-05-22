e9c8a24ef733b768dad333f31172af2c4822f83e
Adrish Bhattacharya <88700100+iadrish@users.noreply.github.com>
2024-05-22 23:13:45 +0530

    Time: 100 ms (46.94%), Space: 60.9 MB (59.80%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string>path;
-        func(0,s,path,result);
-        return result;
-        
+    bool isPalindrome(string s, int i, int j)
+    {
+        while(i<=j)
+        {
+            if(s[i++]!=s[j--])
+                return false;
+        }
+        return true;
     }
-    void func(int index,string s,vector<string> &path,vector<vector<string>> &result)
+    void func(int index,string s, vector<string>&path,vector<vector<string>>&result)
     {
         if(index==s.size())
         {
             result.push_back(path);
-            return;
+            return; 
         }
         for(int i=index;i<s.size();i++)
         {
@@ -21,17 +23,13 @@ public:
                 path.push_back(s.substr(index,i-index+1));
                 func(i+1,s,path,result);
                 path.pop_back();
-                
             }
         }
     }
-    bool isPalindrome(string s,int i, int j)
-    {
-        while(i<=j)
-        {
-            if(s[i++]!=s[j--])
-                return false;
-        }
-        return true;
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>result;
+        vector<string>path;
+        func(0,s,path,result);
+        return result;
     }
 };
